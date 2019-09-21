adb15f5626002fbd9d767a6eee29fe0e0e9d025f
Yusuke Morishita <yusuke.0213@gmail.com>
2019-09-21 17:05:33 +0900

    Update

--- AtCoder/arc061_a.cpp
+++ AtCoder/arc061_a.cpp
@@ -1,47 +1,60 @@
-// SeeAlso: https://arc061.contest.atcoder.jp/tasks/arc061_a
+// SeeAlso: https://atcoder.jp/contests/arc061/tasks/arc061_a
 
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 
-typedef long long ll;
 using namespace std;
 
-int cnt = 0;
-int rec(long long x){
-    if(x == 0) return cnt;
-    else{
-        x /= 10;
-        cnt++;
-        return rec(x);
-    }
-}
- 
-int main(void){
-   ll S;
-   cin >> S;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
 
-   int b = rec(S);
-   ll ans = 0;
-   vector<bool> vec(b-1);
+#define MAX 100000
+#define NIL -1
 
-   for(int i = 0; i < pow(2, b - 1); i++){
-       ll I = i;
-       for(int j = 0; j < b - 1; j++){
-           vec.at(j) = I % 2;
-           I /= 2;
-       }
-    
-        vector<int> array(0);
-        for(int j = 0; j < b-1; j++){
-            if(vec.at(j) == 1) array.push_back(j);
+typedef int _loop_int;
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define FOR(i,a,b) for(_loop_int i=(_loop_int)(a);i<(_loop_int)(b);++i)
+#define FORR(i,a,b) for(_loop_int i=(_loop_int)(b)-1;i>=(_loop_int)(a);--i)
+
+#define DEBUG(x) cout<<#x<<": "<<x<<endl
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i,v.size())cout<<" "<<v[i];cout<<endl
+#define ALL(a) (a).begin(),(a).end()
+
+#define CHMIN(a,b) a=min((a),(b))
+#define CHMAX(a,b) a=max((a),(b))
+
+// 最大公約数
+inline constexpr ll gcd(ll a,ll b){if(!a||!b)return 0;while(b){ll c=b;b=a%b;a=c;}return a;}
+// 最小公倍数
+inline constexpr ll lcm(ll a,ll b){if(!a||!b)return 0;return a*b/gcd(a,b);}
+
+#define print2D(h, w, arr) REP(i, h) { REP(j, w) cout << arr[i][j] << " "; cout << endl; }
+template<class T> void print(const T& x){cout << setprecision(12) << x << endl;}
+template<class T, class... A> void print(const T& first, const A&... rest) { cout << first << " "; print(rest...); }
+
+string S;
+
+int main() {
+    cin >> S;
+
+    int n = S.size();
+    ll ans = 0;
+
+    for (int bit = 0; bit < (1<<(n-1)); ++bit) {
+        ll sum = S[0] - '0';
+        for (int i = 0; i<(n-1); ++i) {
+            if (bit & (1<<i)) { 
+                ans += sum;
+                sum = 0;
+            }         
+            sum *= 10;
+            sum += S[i+1] - '0';
         }
-        int x = array.size();
-    
-        ll current = S;
-        for(int j = 0; j < x; j++){
-            ans += current / ll(pow(10, b-1-array.at(j)));
-            current -= ll(current / int(pow(10, b-1-array.at(j)))) * pow(10, b-1-array.at(j));
-        }
-        ans += ll(current);
+        ans += sum;
     }
-    cout << ans << endl;
+
+    print(ans);
+    return 0;
 }
