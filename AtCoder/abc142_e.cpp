d6a1da7001479cfc45f399cef446d928633049ad
Yusuke Morishita <yusuke.0213@gmail.com>
2019-09-29 10:34:51 +0900

    Update

---
+++ AtCoder/abc142_e.cpp
@@ -0,0 +1,78 @@
+// SeeAlso: https://atcoder.jp/contests/abc142/tasks/abc142_e
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define MAX 100000
+#define NIL -1
+#define MOD 1000000007
+
+typedef int _loop_int;
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define FOR(i,a,b) for(_loop_int i=(_loop_int)(a);i<(_loop_int)(b);++i)
+#define FORR(i,a,b) for(_loop_int i=(_loop_int)(b)-1;i>=(_loop_int)(a);--i)
+
+#define DEBUG(x) cout<<#x<<": "<<x<<endl
+#define DEBUG_VEC(v) cout<<#v<<":";REP(i,v.size())cout<<" "<<v[i];cout<<endl
+#define ALL(a) (a).begin(),(a).end()
+
+#define CHMIN(a,b) a=min((a),(b))
+#define CHMAX(a,b) a=max((a),(b))
+
+// 最大公約数
+inline constexpr ll gcd(ll a,ll b){if(!a||!b)return 0;while(b){ll c=b;b=a%b;a=c;}return a;}
+// 最小公倍数
+inline constexpr ll lcm(ll a,ll b){if(!a||!b)return 0;return a*b/gcd(a,b);}
+
+#define print2D(h, w, arr) REP(i, h) { REP(j, w) cout << arr[i][j] << " "; cout << endl; }
+template<class T> void print(const T& x){cout << setprecision(12) << x << endl;}
+template<class T, class... A> void print(const T& first, const A&... rest) { cout << first << " "; print(rest...); }
+
+
+int main() {
+
+    int N;
+    ll M;
+    ll a[100000], b[100000];
+    ll c[1000][12];    
+    
+    cin >> N >> M;
+    ll max_ans = 0;
+    REP(i, M) {
+        cin >> a[i] >> b[i];
+        max_ans += a[i];
+        REP(j, b[i]) {
+            cin >> c[i][j];
+        }
+    } 
+
+    ll ans = -1;
+    for (int bit = 0; bit < (1<<M); ++bit) {
+
+        set<int> s;
+        ll temp_ans = 0;
+
+        for (int i = 0; i < M; ++i) {
+            if (bit & (1<<i)) {
+                REP(j, b[i]) {
+                    s.insert(c[i][j]);
+                }
+                temp_ans += a[i];
+            }
+        }
+
+        if(s.size()==N) {
+            max_ans = min(max_ans, temp_ans);
+            ans = max_ans;
+        }
+    }
+    print(ans);
+    return 0;
+}
