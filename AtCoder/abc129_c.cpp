21553a55ceffd2e848f3218e80f80fc8686541dd
Yusuke Morishita <yusuke.0213@gmail.com>
2020-05-22 10:30:29 +0900

    Update

--- AtCoder/abc129_c.cpp
+++ AtCoder/abc129_c.cpp
@@ -11,29 +11,66 @@ typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
 
 #define MAX 100000
+#define NIL -1
+#define MOD 1000000007
 
 typedef int _loop_int;
-#define REP(i,n) for(int i = 0; i < n; i++)
+#define rep(i,n) for(int i = 0; i < n; i++)
 #define FOR(i,a,b) for(_loop_int i=(_loop_int)(a);i<(_loop_int)(b);++i)
 #define FORR(i,a,b) for(_loop_int i=(_loop_int)(b)-1;i>=(_loop_int)(a);--i)
 
-#define DEBUG(x) cout<<#x<<": "<<x<<endl
-#define DEBUG_VEC(v) cout<<#v<<":";REP(i,v.size())cout<<" "<<v[i];cout<<endl
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define debig_vec(v) cout<<#v<<":";rep(i,v.size())cout<<" "<<v[i];cout<<endl
 #define ALL(a) (a).begin(),(a).end()
-
-#define CHMIN(a,b) a=min((a),(b))
-#define CHMAX(a,b) a=max((a),(b))
 
 // 最大公約数
 inline constexpr ll gcd(ll a,ll b){if(!a||!b)return 0;while(b){ll c=b;b=a%b;a=c;}return a;}
 // 最小公倍数
 inline constexpr ll lcm(ll a,ll b){if(!a||!b)return 0;return a*b/gcd(a,b);}
 
-#define print2D(h, w, arr) REP(i, h) { REP(j, w) cout << arr[i][j] << " "; cout << endl; }
+#define print2D(h, w, arr) rep(i, h) { rep(j, w) cout << arr[i][j] << " "; cout << endl; }
 template<class T> void print(const T& x){cout << setprecision(12) << x << endl;}
 template<class T, class... A> void print(const T& first, const A&... rest) { cout << first << " "; print(rest...); }
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
 
 int main() {
-    
+    ll n, m;
+    cin >> n >>m;
+    ll a[m];
+ 
+    rep(i, m) {
+        cin >> a[i];
+    }
+
+    ll dp[n+1];
+    memset(dp, 0, sizeof(dp));
+    dp[0] = 1;
+    ll index = 0;
+    FOR(i, 1, n+1) {
+        while(index<m && a[index]<i) {
+            index++;
+        }
+
+        if(a[index]==i) continue;
+        dp[i] += dp[i-1];
+        dp[i] %= MOD;
+        if (i>1) dp[i] += dp[i-2];
+        dp[i] %= MOD;
+    }
+
+    print(dp[n]);
     return 0;
 }
