c49f532ffff4f75febd712cf8c338e7d27babda9
Yusuke Morishita <yusuke.0213@gmail.com>
2020-06-10 14:43:07 +0900

    Update

--- AtCoder/arc029_1.cpp
+++ AtCoder/arc029_1.cpp
@@ -1,39 +1,65 @@
-// SeeAlso: https://atcoder.jp/contests/arc029/tasks/arc029_1
+// SeeAlso: 
 
 #include <bits/stdc++.h>
 
-#define MAX 5
-#define rep(i,n) for(int i=0;i<n;i++)
-
-const int INF = 1e9;
-
 using namespace std;
 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define MAX 100000
+#define NIL -1
+#define MOD 1000000007
+
+typedef int _loop_int;
+#define rep(i,n) for(int i = 0; i < n; i++)
+#define FOR(i,a,b) for(_loop_int i=(_loop_int)(a);i<(_loop_int)(b);++i)
+#define FORR(i,a,b) for(_loop_int i=(_loop_int)(b)-1;i>=(_loop_int)(a);--i)
+
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define debig_vec(v) cout<<#v<<":";rep(i,v.size())cout<<" "<<v[i];cout<<endl
+#define ALL(a) (a).begin(),(a).end()
+
+// 最大公約数
+inline constexpr ll gcd(ll a,ll b){if(!a||!b)return 0;while(b){ll c=b;b=a%b;a=c;}return a;}
+// 最小公倍数
+inline constexpr ll lcm(ll a,ll b){if(!a||!b)return 0;return a*b/gcd(a,b);}
+
+#define print2D(h, w, arr) rep(i, h) { rep(j, w) cout << arr[i][j] << " "; cout << endl; }
+template<class T> inline void print(const T& x){cout << setprecision(12) << x << endl;}
+template<class T, class... A> inline void print(const T& first, const A&... rest) { cout << first << " "; print(rest...); }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+const long long INF = 1LL<<60;
+
 int main() {
-    int N;
-    int t[MAX];
+    ll n;
+    cin >> n;
+    vector<int> t;
+    rep(i,n) {
+        ll temp; 
+        cin >> temp;
+        t.push_back(temp);
+    }
+
+    sort(t.begin(), t.end(), greater<int>());
     
-    cin >> N;
-
-    for (int i = 0; i < N; i++) {
-        cin >> t[i];
-    }
-
-    int ans = INF;
-
-    for (int bit = 0; bit <= (1<<4); bit++) {
-        int a = 0;
-        int b = 0;
-        rep(i,4){
-            if (bit & (1 << i)) {
-                a += t[i];
-            } else {
-                b += t[i];
-            }
+    ll time1 = 0;
+    ll time2 = 0;
+    rep(i, n) {
+        if(time1<=time2) {
+            time1 += t.front();
+        } else {
+            time2 += t.front();
         }
-        ans =  min(ans, max(a, b));
+        t.erase(t.begin());
     }
-
-    cout << ans << endl;
+    debug(time1);
+    debug(time2);
+    print(max(time1, time2));
     return 0;
 }
