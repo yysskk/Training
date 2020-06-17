e01861785ddcbd91c9d10ceaad854fc929c5d5be
Yusuke Morishita <yusuke.0213@gmail.com>
2020-06-17 15:15:44 +0900

    Update

---
+++ AtCoder/abc170_c.cpp
@@ -0,0 +1,70 @@
+// SeeAlso: 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define MAX 100000
+#define NIL -1
+#define MOD 1000000007
+
+typedef int _loop_int;
+#define rep(i,n) for(int i = 0; i < n; i++)
+#define FOR(i,a,b) for(_loop_int i=(_loop_int)(a);i<(_loop_int)(b);++i)
+#define FORR(i,a,b) for(_loop_int i=(_loop_int)(b)-1;i>=(_loop_int)(a);--i)
+
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define debig_vec(v) cout<<#v<<":";rep(i,v.size())cout<<" "<<v[i];cout<<endl
+#define ALL(a) (a).begin(),(a).end()
+
+// 最大公約数
+inline constexpr ll gcd(ll a,ll b){if(!a||!b)return 0;while(b){ll c=b;b=a%b;a=c;}return a;}
+// 最小公倍数
+inline constexpr ll lcm(ll a,ll b){if(!a||!b)return 0;return a*b/gcd(a,b);}
+
+#define print2D(h, w, arr) rep(i, h) { rep(j, w) cout << arr[i][j] << " "; cout << endl; }
+template<class T> inline void print(const T& x){cout << setprecision(12) << x << endl;}
+template<class T, class... A> inline void print(const T& first, const A&... rest) { cout << first << " "; print(rest...); }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+const long long INF = 1LL<<60;
+
+int main() {
+    ll x, n;
+    cin >> x >> n;
+    ll p[100];
+    memset(p, 0, sizeof(p));
+    ll ans;
+    ll length = 200;
+    rep(i, n) {
+        ll t;
+        cin >> t;
+        p[i] += 1;
+        ans = max(ans, p[i]);
+        t = abs(x-p[i]);
+        length = min(length, t);
+    };
+
+    ll temp = ans+x;
+    length *= 2;
+    rep(i,length) {
+        if(x>=i && p[x-i]==0) {
+            print(x-i);
+            return 0;
+        }
+
+        if(p[x+i]==0) {
+            print(x+i);
+            return 0;
+        }
+    }
+    print(ans);
+    return 0;
+}
